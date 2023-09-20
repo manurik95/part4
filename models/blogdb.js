8759f1a54bc1191babd106fae522bab1a644ab51
manurik95 <142829913+manurik95@users.noreply.github.com>
2023-09-20 18:00:30 +0200

    part4

---
+++ models/blogdb.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author:{ 
+        type: String,
+        required: true
+    },
+    url:{ 
+        type: String,
+        required: true
+    },
+    likes:{ 
+        type: Number,
+    }
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+		delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('blogs', blogSchema)
